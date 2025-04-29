d2b21c64111b3c6560a50beb29eb6d0ea540890a
Aya Hamdy <ayah39813@gmail.com>
2025-04-29 11:52:39 +0200

    Add frontend folder

---
+++ exam-system/frontend/src/app/pages/admin/student-results/student-results.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-results',
+  imports: [],
+  templateUrl: './student-results.component.html',
+  styleUrl: './student-results.component.css'
+})
+export class StudentResultsComponent {
+
+}
---
+++ exam-system/frontend/src/app/pages/take-exam/take-exam.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-take-exam',
+  imports: [],
+  templateUrl: './take-exam.component.html',
+  styleUrl: './take-exam.component.css'
+})
+export class TakeExamComponent {
+
+}
