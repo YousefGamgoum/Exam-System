c4b35a2396d9318d227fafdf543ed7995b88e416
YousefGamgoum <yousefsalah959@gmail.com>
2025-05-02 10:10:16 +0300

    commit

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -1,28 +1,13 @@
-const { json } = require("express");
-const jwt = require("jsonwebtoken");
-const AppError = require("../utils/AppError");
-const { catchAsync } = require("../utils/catchAsync");
 
-exports.auth = catchAsync(function (req, res, next) {
-  const { authorization } = req.headers;
+const AppError = require("../utils/appError");
 
-  if (!authorization || !authorization.startsWith("Bearer "))
-    return next(new AppError(401, "Please login first"));
 
-  //   Token verification
-  const token = authorization.split(" ")[1];
-  let decoded = jwt.verify(token, process.env.SECRET);
-  console.log(decoded);
-
-  req.id = decoded.id;
-  req.role = decoded.role;
-  next();
-});
-
-exports.restrictTo = (...roles) => {
+exports.userRoleDetect = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.role))
-      return next(new AppError(403, "you don't have permission"));
-    else next();
+    if (!roles.includes(req.role)) {
+      return next(new AppError(403, "You don't have permission"));
+    } else {
+      next();
+    }
   };
 };
