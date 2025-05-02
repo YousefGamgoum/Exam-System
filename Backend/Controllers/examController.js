4cb5211057c19f267e7f63db8efb58529b136313
YousefGamgoum <yousefsalah959@gmail.com>
2025-05-02 09:58:58 +0300

    Commit

--- Backend/Controllers/examController.js
+++ Backend/Controllers/examController.js
@@ -1,9 +1,8 @@
-const { catchAsync } = require("../utils/catchAsync");
-
 const Exam = require("../Models/Exams");
 const Result = require("../Models/Results");
-const User = require("../Models/Users");
-const AppError = require("../utils/appError");
+const AppError = require("../Utils/AppError");
+const { catchAsync } = require("../Utils/catchAsync");
+const User = require("../Models/user");
 
 exports.getExamdetails = catchAsync(async (req, res, next) => {
   const { id } = req.params;
@@ -76,12 +75,53 @@ exports.updateExamById = catchAsync(async (req, res, next) => {
   });
 });
 
+// get all exams
+exports.getAllExams = catchAsync(async (req, res, next) => {
+  // Get all exams
+  const exams = await Exam.find().select("name description totalMarks");
+
+  if (!exams.length) {
+    return next(new AppError(404, "No available exams"));
+  }
+
+  // If the user is an admin, return all exams
+  if (req.role === "admin") {
+    return res.status(200).json({ status: "success", data: exams });
+  }
+
+  // For students, filter out exams they have already taken
+  const userResults = await Result.find({ user: req.id }).select("exam");
+  const takenExamIds = userResults.map((result) => result.exam.toString());
+
+  // Filter exams to exclude the ones the student has taken
+  const availableExams = exams.filter(
+    (exam) => !takenExamIds.includes(exam._id.toString())
+  );
+
+  if (!availableExams.length) {
+    return next(new AppError(404, "No available exams for you"));
+  }
+
+  res.status(200).json({ status: "success", data: availableExams });
+});
+
+// Get exam by ID
+exports.getExamById = catchAsync(async (req, res, next) => {
+  const exam = await Exam.findById(req.params.id);
+  if (!exam) {
+    return next(new AppError(404, "Exam not found"));
+  }
+
+  res.status(200).json({ status: "success", data: exam });
+});
+
 exports.deleteExamById = catchAsync(async (req, res, next) => {
   const { id } = req.params;
   const exam = await Exam.findByIdAndDelete(id);
   if (!exam) {
     return next(new AppError(404, "Exam not found"));
   }
+
   res.status(200).json({
     status: "success",
     message: "Exam deleted successfully",
@@ -151,4 +191,39 @@ exports.getExams = catchAsync(async (req, res, next) => {
       examResults: examResults,
     },
   });
+});
+
+// Submit exam answers and calculate score
+exports.submitExam = catchAsync(async (req, res, next) => {
+  const { examId, answers } = req.body;
+
+  // Check if user is authenticated
+  if (!req.id) {
+    return next(new AppError(401, "Please login to submit the exam"));
+  }
+
+  const exam = await Exam.findById(examId);
+  if (!exam) {
+    return next(new AppError(404, "Exam not found"));
+  }
+
+  let score = 0;
+  exam.questions.forEach((question, index) => {
+    const userAnswer = answers.find((ans) => ans.questionIndex === index);
+    if (userAnswer) {
+      const selectedChoice = question.choices[userAnswer.selectedChoiceIndex];
+      if (selectedChoice && selectedChoice.isCorrect) {
+        score += question.marks;
+      }
+    }
+  });
+
+  // Save the result
+  const result = await Result.create({
+    user: req.id,
+    exam: examId,
+    score,
+  });
+
+  res.status(201).json({ status: "success", data: { score, result } });
 });
--- Backend/utils/catchAsync.js
+++ Backend/utils/catchAsync.js
@@ -1,6 +1,5 @@
 exports.catchAsync = (fn) => {
   return (req, res, next) => {
-    // fn(req, res, next).catch(next);
-    Promise.resolve(fn(req, res, next)).catch(next);
+    fn(req, res, next).catch(next);
   };
 };
